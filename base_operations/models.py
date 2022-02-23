5afdae79c3b62df2e9578027dea1a88260896cba
stonerjack <jack@drummer.ru>
2022-02-23 19:22:48 +0300

    more apps

---
+++ base_operations/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class operations(models.Model):
+    a = models.BigIntegerField()
+    b = models.BigIntegerField()
+    c = models.BigIntegerField()
+
--- landing/settings.py
+++ landing/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'crm.apps.CrmConfig'
+    'base_operations.apps.BaseOperationsConfig',
+    'crm.apps.CrmConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
